ffbfa60e45e4db9b8a701c8fa0812959174dbabd
Anouk ZOUAOUI <anzouaoui78@yahoo.fr>
2017-07-23 17:58:09 +0200

    Modification du fichier gulpfile

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -6,44 +6,37 @@ var concat =  require('gulp-concat');
 var uglify = require('gulp-uglify');
 var cleanCss = require('gulp-clean-css');
 var rename = require('gulp-rename');
-var clean = require('gulp-clean');
+var clean = require('gulp-contrib-clean');
+var sourcemaps = require('gulp-sourcemaps');
 
-//Concaténation des fichiers JS
-gulp.task('concat-js', function () {
+//JAVASCRIPT
+gulp.task('javascript', function () {
    return gulp.src('js/*')
+       //Concaténation
        .pipe(concat('main.js'))
-       .pipe(gulp.dest('dist/js'));
-});
-
-//Concaténation des fichiers CSS
-gulp.task('concat-css', function () {
-   return gulp.src('css/*')
-       .pipe(concat('main.css'))
-       .pipe(gulp.dest('dist/css'));
-});
-
-//Minification du fichier main.js
-gulp.task('minify-js', function () {
-   console.log('task: minify-js');
-   return gulp.src('dist/js/main.js')
+       .pipe(gulp.dest('dist/js'))
+       //Minification
        .pipe(uglify())
        .pipe(rename({ suffix: '.min' }))
        .pipe(gulp.dest('dist/js'));
 });
 
-//Minification du fichier main.css
-gulp.task('minify-css', function () {
-   console.log('task: minify-css');
-   return gulp.src('dist/css/main.css')
+//CSS
+gulp.task('css', function () {
+   return gulp.src('css/*')
+       //Concaténation
+       .pipe(concat('main.css'))
+       .pipe(gulp.dest('dist/css'))
+       //Minification
        .pipe(cleanCss({ compatibility: 'ie8' }))
        .pipe(rename({ suffix: '.min' }))
        .pipe(gulp.dest('dist/css'))
 });
 
-//Clean
+//CLEAN
 gulp.task('clean', function () {
-    return gulp.src('dist/', {read: false})
+    gulp.src('dist')
         .pipe(clean());
 });
 
-gulp.task('default', ['clean', 'concat-js', 'concat-css', 'minify-js',  'minify-css']);
+gulp.task('default', ['clean', 'javascript', 'css']);
